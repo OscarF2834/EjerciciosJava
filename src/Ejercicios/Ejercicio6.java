f6ca99bd26b2d2ed71c7c3c2bd4360f8aaf406bc
OscarF2834 <pipesegoviaalvarado@gmail.com>
2025-03-26 04:53:24 -0500

    Segundo intento

--- src/Ejercicios/Ejercicio6.java
+++ src/Ejercicios/Ejercicio6.java
@@ -1,4 +1,16 @@
 package Ejercicios;
 
 public class Ejercicio6 {
+    public static void main(String[] args) {
+        System.out.println("Ejercicio numero 6 de logica de programacion con java");
+        //Declaracion de las variables.
+        int numero;
+        float suma = 0;
+        for (int i=1; i<20; i=i+2){
+            //El cuerpo del ciclo
+            System.out.println("Los numeros son: " + i);
+            suma += i;
+        }
+        System.out.println("La suma de estos numeros es de: " + suma);
+    }
 }
